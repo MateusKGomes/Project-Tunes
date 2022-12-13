4b5770659c3f0245ee16b8781037fad91162aef3
Mateus Kulik <mateuskulik30@gmail.com>
2022-12-13 14:57:26 -0300

    Iniciando o projeto Trybetunes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+      <Route />
+      </BrowserRouter>
+    );
   }
 }
 
