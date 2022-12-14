a56814fb602822ea800174b1194a9399d7b62d6f
Mateus Kulik <mateuskulik30@gmail.com>
2022-12-14 08:52:52 -0300

    Requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,11 @@ class Header extends Component {
           <ul>
             <li>
               <Link to="/search" data-testid="link-to-search">Search</Link>
+            </li>
+            <li>
               <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+            </li>
+            <li>
               <Link to="/profile" data-testid="link-to-profile">Profile</Link>
             </li>
           </ul>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,55 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    search: '',
+    buttonDisabled: true,
+
+  };
+
+  validationFields = () => {
+    const { search } = this.state;
+    const TWO = 2;
+    const validationSearch = search.length >= TWO;
+
+    this.setState({
+      buttonDisabled: !(validationSearch),
+    });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }), this.validationFields);
+  };
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
-        <h3>Estou em Search</h3>
+        <h3>Pesquise seu artista</h3>
         <Header />
+
+        <div>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="search"
+            placeholder="Pesquisar artista"
+            id="search"
+            onChange={ this.handleChange }
+          />
+
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ buttonDisabled }
+            // onClick={ this.buttonClickLogin }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
