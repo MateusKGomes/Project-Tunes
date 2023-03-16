ad1dbeb9f174e61ab272fa7898405ff72f7b962b
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-03-16 11:38:17 -0300

    remove links sem uso

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,12 +40,6 @@ class Header extends Component {
             <li>
               <Link to="/search" data-testid="link-to-search">Search</Link>
             </li>
-            <li>
-              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
-            </li>
-            <li>
-              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
-            </li>
           </ul>
         </header>
 
